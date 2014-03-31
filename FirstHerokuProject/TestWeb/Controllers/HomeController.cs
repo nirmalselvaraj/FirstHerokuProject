7359dcb6ba487410afbf1a0405f39340cd64e82b
Nirmal Selvaraj <nselvaraj@ancestry.com>
2014-03-31 11:40:56 -0700

    changing view bag title

--- FirstHerokuProject/TestWeb/Controllers/HomeController.cs
+++ FirstHerokuProject/TestWeb/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TestWeb.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page for Nirmal";
 
             return View();
         }
